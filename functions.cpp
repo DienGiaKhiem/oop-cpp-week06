12cd7612416a9d469cacf24658f09404db038488
Dien Gia Khiem <25AD11006@hcmute.edu.vn>
2026-09-11 10:11:45 +0700

    Initial commit

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void showWelcome(){
+    cout << "==============================" << endl;
+    cout <<" Welcome to the student manager" << endl;
+    cout << "==============================" << endl;
+}
+
+int main(){
+    showWelcome();
+    cout << "Let's get started!" << endl;
+    return 0;   
+}
